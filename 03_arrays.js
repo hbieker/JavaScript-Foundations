3bcb72b8113895b9ce5fadf19182df4e15d244be
Heath Bieker <hbieker@gmail.com>
2022-04-28 15:29:11 -0600

    Final Array Work

--- 03_arrays.js
+++ 03_arrays.js
@@ -16,3 +16,19 @@ grades.pop() //20, 73, 89, 98, 56, 78 (removes from end)
 console.log(grades)
 grades.unshift(0) //0, 20, 73, 89, 98, 56, 78 (adds to beginning)
 console.log(grades)
+console.log(grades.indexOf(78)) //Tell me index for value 78
+console.log(grades.includes(120)) //Tell me if 120 is present
+
+subgrades = grades.slice(2,5) //Return a portion of the array
+console.log(subgrades)
+
+var sum = 0
+var avg = 0
+for(let i=0; i<grades.length; i++)
+{
+    //console.log(grades[i])
+    sum = sum + grades[i]
+}
+console.log(sum)
+avg = sum / grades.length
+console.log(avg)
