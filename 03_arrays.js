6f1d391614745d701e375df6b221e4f73fa91401
Heath Bieker <hbieker@gmail.com>
2022-04-27 18:06:29 -0600

    Updated arrays file

--- 03_arrays.js
+++ 03_arrays.js
@@ -10,5 +10,9 @@ grades[0] = 20 //assign index 0 to 20
 console.log(grades) //20, 73, 89, 98, 56, 78
 
 console.log(grades.length) //length method (6)
-grades.push(100) //20, 73, 89, 98, 56, 78, 100
+grades.push(100) //20, 73, 89, 98, 56, 78, 100 (adds 100 to end)
+console.log(grades)
+grades.pop() //20, 73, 89, 98, 56, 78 (removes from end)
+console.log(grades)
+grades.unshift(0) //0, 20, 73, 89, 98, 56, 78 (adds to beginning)
 console.log(grades)
